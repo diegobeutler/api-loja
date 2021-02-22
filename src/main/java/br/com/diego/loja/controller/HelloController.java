b21b08b2a89adb1815962eb2451866296486007b
diego <diegobeutler2411@gmail.com>
2021-02-22 10:32:00 -0300

    banco

--- src/main/java/br/com/diego/loja/controller/HelloController.java
+++ src/main/java/br/com/diego/loja/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller //spring consegue encontrar essa classe
 public class HelloController {
 
-    @RequestMapping("/")  // diz quando vai ser chamado
+    @RequestMapping("/")  /// diz quando vai ser chamado
     @ResponseBody // para não navegar para uma página, caso contrário o spring vai pensar que é uma
     public void executa() {
         System.out.println( "olá mundo");
